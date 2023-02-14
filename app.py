53202ccbf69bdfd0ab6dc3e7dea5f839de1bf326
aashish-1904 <akumar7_be20@thapar.edu>
2023-02-14 10:15:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def download_audio_from_search(singer, n, m):
                 audio = pydub.AudioSegment.from_file(f"{singer}_{i}.mp4")
                 audio = audio[:n * 1000]
                 audio.export(f"{singer}_{i}.mp3", format="mp3")
-                success = True/
+                success = True
         
 #         except pytube.exceptions.VideoUnavailable as e:
             except:
