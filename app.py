5216d775d3a255dcf8bd14eb0488ad895deecfc0
aashish-1904 <akumar7_be20@thapar.edu>
2023-02-14 10:54:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,23 +23,24 @@ def download_audio_from_search(singer, n, m):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     for i, result in enumerate(results):
         video_url = "https://www.youtube.com/watch?v=" + result["id"]
-        success = False
-        while not success:
-            try:
-                session = requests.Session()
-                yt = YouTube(session.get(video_url, headers=headers).text)
-                stream = yt.streams.filter(only_audio=True).first()
-                stream.download(filename=f"{singer}_{i}.mp4")
+#         success = False
+#         while not success:
+        try:
+            session = requests.Session()
+            yt = YouTube(session.get(video_url, headers=headers).text)
+            stream = yt.streams.filter(only_audio=True).first()
+            stream.download(filename=f"{singer}_{i}.mp4")
 
-                audio = pydub.AudioSegment.from_file(f"{singer}_{i}.mp4")
-                audio = audio[:n * 1000]
-                audio.export(f"{singer}_{i}.mp3", format="mp3")
-                st.write(f"Audio of {n} seconds from {yt.title} downloaded and converted to MP3 successfully")
-                success = True
+            audio = pydub.AudioSegment.from_file(f"{singer}_{i}.mp4")
+            audio = audio[:n * 1000]
+            audio.export(f"{singer}_{i}.mp3", format="mp3")
+            st.write(f"Audio of {n} seconds from {yt.title} downloaded and converted to MP3 successfully")
+#                 success = True
         
-            except pytube.exceptions.VideoUnavailable as e:
+        except: # pytube.exceptions.VideoUnavailable as e
 #             except:
-                time.sleep(2)
+            time.sleep(2)
+            continue
 #             continue
 # def download_audio_from_search(singer, n, m):
 #     results = YoutubeSearch(singer, max_results=m).to_dict()
